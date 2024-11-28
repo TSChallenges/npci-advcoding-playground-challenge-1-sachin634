0291d71d311ea23513696b4ee688e5db214ce118
sachin634 <78214246+sachin634@users.noreply.github.com>
2024-11-28 23:35:31 +0530

    PlayGround Challange done

--- StockPortfolio.java
+++ StockPortfolio.java
@@ -4,26 +4,37 @@ class StockPortfolio {
     private int quantity;
     private double buyingPrice;
     private double currentPrice;
-
+    private double highestPrice;
     // TODO: Initialize stock details
     public StockPortfolio(String symbol, int quantity, double buyingPrice) {
         // TODO
+          this.symbol = symbol;
+        this.quantity = quantity;
+        this.buyingPrice = buyingPrice;
+        this.currentPrice = buyingPrice;
+        this.highestPrice = buyingPrice;
     }
 
     // TODO: Update current market price
     public void updatePrice(double newPrice) {
         // TODO
+         if(newPrice <0)
+              throw new ArithmeticException("newPrice should be positive");
+          currentPrice = newPrice;
+          if(newPrice > highestPrice)
+              highestPrice = newPrice;
     }
 
     // TODO: Calculate total profit or loss
     public double calculateProfit() {
         // TODO
-        return 0.0;
+       double calculate = (currentPrice - buyingPrice )/quantity;
+        return calculate;
     }
 
     // TODO: Calculate current total value of holdings
     public double getCurrentValue() {
         // TODO
-        return 0.0;
+       return currentPrice/quantity;
     }
 }
